d892a4169834e67632207f2988c34b336db178b2
Ramon Xavier <ramon1234rxm@gmail.com>
2021-07-15 23:18:09 -0300

    Create a request to user data and repos

--- src/infrastructure/services/user.js
+++ src/infrastructure/services/user.js
@@ -1,14 +1,26 @@
 import axios from "axios";
 
 const baseURL = "https://api.github.com/users";
+
 const getUser = async (user) => {
   try {
     const request = await axios.get(`${baseURL}/${user}`);
-    const response = request;
-    return response;
+
+    const response = await request;
+    return response.data;
   } catch (error) {
     throw new Error(error);
   }
 };
 
-export { getUser };
+const getRepo = async (user) => {
+  try {
+    const request = await axios.get(`${baseURL}/${user}/repos`);
+    const response = await request;
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export { getUser, getRepo };
