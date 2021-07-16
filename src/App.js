e2afe0b2c1ac056f5c42d6d94ece7d74d6926dc3
Ramon Xavier <ramon1234rxm@gmail.com>
2021-07-15 23:18:28 -0300

    Provider in Routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import GlobalStyle from "./styles/Global";
 
+import { UserContextProvider } from "./contexts/UserContext";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 
@@ -10,11 +11,11 @@ const App = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <>
+        <UserContextProvider>
           <GlobalStyle />
           <Route path="/" exact component={Login} />
           <Route path="/profile" component={Profile} />
-        </>
+        </UserContextProvider>
       </Switch>
     </BrowserRouter>
   );
