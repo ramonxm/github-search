7662c9812241864460aa922c6619677751b773ae
Ramon Xavier <ramon1234rxm@gmail.com>
2021-07-15 23:19:01 -0300

    Make a request and set data e repo

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,20 +1,51 @@
-import React from "react";
+import { useCallback } from "react";
 import { useHistory } from "react-router";
+import { useUser } from "../../hooks/useUser";
+import { getRepo, getUser } from "../../infrastructure/services/user";
 import * as S from "./style";
 
 const Login = () => {
   const history = useHistory();
+  const { setData, user, setUser, setRepo } = useUser();
 
-  const handleLogin = () => {
-    history.push("/profile");
-  };
+  const handleLoginUser = useCallback(
+    async (user) => {
+      const response = await getUser(user);
+      setData(response);
+      localStorage.setItem("users", JSON.stringify(response));
+      history.push("/profile");
+    },
+    // eslint-disable-next-line
+    [user]
+  );
+
+  const handleGetRepo = useCallback(
+    async (user) => {
+      const response = await getRepo(user);
+      setRepo(response);
+      localStorage.setItem("repo", JSON.stringify(response));
+    },
+    // eslint-disable-next-line
+    [user]
+  );
 
   return (
     <S.ContainerLogin>
       <S.LogoImg src="/assets/svg/logo-vertical.svg" alt="Logo" />
       <S.ContainerSearch>
-        <S.InputSearch type="text" placeholder="Enter your user name" />
-        <S.SubmitButton type="submit" onClick={handleLogin}>
+        <S.InputSearch
+          type="text"
+          placeholder="Enter your user name"
+          onChange={(e) => setUser(e.target.value)}
+          value={user}
+        />
+        <S.SubmitButton
+          type="submit"
+          onClick={() => {
+            handleGetRepo(user);
+            handleLoginUser(user);
+          }}
+        >
           Search
         </S.SubmitButton>
       </S.ContainerSearch>
