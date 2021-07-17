9f632756df3ebc0fb0413e52968fa31d4f79e001
Ramon Xavier <ramon1234rxm@gmail.com>
2021-07-16 21:22:36 -0300

    Create a Router component and animations

---
+++ src/infrastructure/Routes.js
@@ -0,0 +1,33 @@
+import { Route, useLocation, Switch } from "react-router-dom";
+import { useTransition, animated } from "react-spring";
+
+import { Login } from "../pages/Login";
+import { Profile } from "../pages/Profile";
+
+const Routes = () => {
+  const location = useLocation();
+  const transitions = useTransition(location, {
+    from: {
+      opacity: 0,
+      transform: "translate(100%,0)",
+    },
+    enter: {
+      opacity: 1,
+      transform: "translate(0%,0)",
+    },
+    leave: {
+      opacity: 0,
+      transform: "translate(-50%,0)",
+    },
+  });
+  return transitions((props, items) => (
+    <animated.div style={props}>
+      <Switch location={items}>
+        <Route path="/" exact component={Login} />
+        <Route path="/profile" component={Profile} />
+      </Switch>
+    </animated.div>
+  ));
+};
+
+export { Routes };
