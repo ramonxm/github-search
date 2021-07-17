dc2e5611ce4a30430cb594f2d9ef19b4aad13a46
Ramon Xavier <ramon1234rxm@gmail.com>
2021-07-16 22:36:58 -0300

    Refactoring useUser

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -2,10 +2,7 @@ import { useContext } from "react";
 import { UserContext } from "../contexts/UserContext";
 
 const useUser = () => {
-  const { data, setData, user, setUser, repo, setRepo } =
-    useContext(UserContext);
-
-  return { data, setData, user, setUser, repo, setRepo };
+  return { ...useContext(UserContext) };
 };
 
 export { useUser };
