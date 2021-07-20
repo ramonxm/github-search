3a97678c9adccb4685ad9ce629407d4095e439c9
Ramon Xavier <ramon1234rxm@gmail.com>
2021-07-20 16:39:53 -0300

    Create a deslogged function

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,14 @@
 import { Link } from "react-router-dom";
+import { useUser } from "../../hooks/useUser";
 import * as S from "./style";
 const Header = () => {
+  const { setValues } = useUser();
+
+  const handleDeslogged = () => {
+    setValues(false);
+    localStorage.removeItem("users");
+    localStorage.removeItem("repo");
+  };
   return (
     <S.ContainerHeader>
       <Link to="/profile">
@@ -9,7 +17,7 @@ const Header = () => {
           alt="Github Search"
         />
       </Link>
-      <Link to="/">
+      <Link to="/" onClick={handleDeslogged}>
         <S.NavigationImg src="/assets/images/row.png" alt="Github Search" />
       </Link>
     </S.ContainerHeader>
